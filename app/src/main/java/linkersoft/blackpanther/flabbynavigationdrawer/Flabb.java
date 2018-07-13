4d32c35fe303d2a524e77b4de7fb23af02e8fb37
LiNKeR <am.usa.to.be@gmail.com>
2018-07-13 01:07:01 +0100

    Initial commit

---
+++ app/src/main/java/linkersoft/blackpanther/flabbynavigationdrawer/Flabb.java
@@ -0,0 +1,13 @@
+package linkersoft.blackpanther.flabbynavigationdrawer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Flabb extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.flabb);
+    }
+}
